afed5807259f99f523590f0091313a255c3eda34
Aniket Kumar <aniket.kumar@coupa.com>
2024-12-06 12:20:21 +0530

    Saving data to Database with error messages enabled

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -8,11 +8,29 @@ class BlogsController < ApplicationController
     @blogs = Blog.all 
   end
 
-  def new  
+  def new
+    # When we redirect from create route then only we have @blog, but in out new.html.erb file we don't have @blog for the first
+    # time, so we create a blog instance here to get rid to that edge case
+    
+    @blog = Blog.new()
   end
 
   def create
-    render plain: params[:blog]
+    # render plain: params[:blog]
+    
+    # @blog = Blog.new(params[:blog]) <- This doesn't work because from Rails 4 onwards it does the security check for what data
+    # is comming from the web.
+    
+    @blog = Blog.new(params.require(:blog).permit(:title, :description))
+    # render plain: @blog.inspect
+    
+    if @blog.save
+      # After saving to the database rails doesn't know what to do, so we redirect ruby to that blog which is created.
+      redirect_to blog_path(@blog)
+    else
+      render "new"
+    end
+    
   end
 
 end
