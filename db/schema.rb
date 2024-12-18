fb6c2681e16201d175aad64892b81280e699efe1
Aniket Kumar <aniket.kumar@coupa.com>
2024-12-18 10:56:16 +0530

    Add user table and user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_12_05_053902) do
+ActiveRecord::Schema[8.0].define(version: 2024_12_18_051623) do
   create_table "blogs", force: :cascade do |t|
     t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.text "description"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
